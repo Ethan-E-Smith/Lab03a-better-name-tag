aae7dddeb39e62edc29339646c3b4c6949b3356c
Ethan-E-Smith <ethanessix@yahoo.com>
2021-08-12 01:56:50 -0500

    textbox added

---
+++ app.js
@@ -0,0 +1,15 @@
+const myInput = document.getElementById ('my-input');
+
+const myButton = document.getElementById ('my-button');
+
+const myInjectionSpace = document.getElementById ('my-injection-space');
+
+myButton.addEventListener('click', () => {
+    nameEl.text = inputEl.value;
+
+}):
+
+
+
+
+
